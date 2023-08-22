4f6185b3bb7f3c44e49ea314530c5e41ceff52ec
Karan Joshi <Karan190213@gmail.com>
2023-08-22 13:57:56 +0530

    Add Current Date Display Using Luxon Library
    In this commit, I've implemented the functionality to display the current date below the navigation bar using the Luxon library.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import { BookManager } from './modules/BookManager.js';
+import { DateTime } from './luxon.js';
 
 const bookManagerDisplay = new BookManager();
-bookManagerDisplay();
+bookManagerDisplay.displayBooks();
+
+// UTC +1 program time zone!
+
+const dateAndTime = () => {
+    const now = DateTime.local();
+    const formattedDateTime = now.toFormat('LLLL dd, yyyy HH:mm:ss');
+    document.getElementById('date-time').textContent = formattedDateTime;
+  };
+  
+  dateAndTime();
+  setInterval(dateAndTime, 1000);
